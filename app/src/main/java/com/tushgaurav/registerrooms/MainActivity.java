9435b685f0286821ab8ef6ebe15ffad82cb14d60
Tushar <tushar.gaurav416@gmail.com>
2021-08-28 21:23:21 +0530

    added better layout

--- app/src/main/java/com/tushgaurav/registerrooms/MainActivity.java
+++ app/src/main/java/com/tushgaurav/registerrooms/MainActivity.java
@@ -1,14 +1,115 @@
 package com.tushgaurav.registerrooms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editName, editPhone, editEmail;
+    private TextView txtWarnName, txtWarnEmail, txtWarnMobile, txtWarnAgreement, txtAgreement;
+    private Button btnSubmit, btnReset;
+    private ConstraintLayout parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+
+
+        // click listeners
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               initRegister();
+            }
+        });
+    }
+
+    private void initViews() {
+        parent = findViewById(R.id.parent);
+
+        editName = findViewById(R.id.editName);
+        editEmail = findViewById(R.id.editEmail);
+        editPhone = findViewById(R.id.editPhone);
+
+        txtWarnName = findViewById(R.id.txtWarnName);
+        txtWarnEmail = findViewById(R.id.txtWarnEmail);
+        txtWarnMobile = findViewById(R.id.txtWarnMobile);
+        txtWarnAgreement = findViewById(R.id.txtWarnAgreement);
+
+        txtAgreement = findViewById(R.id.txtAgreement);
+
+        btnReset = null;
+        btnSubmit = findViewById(R.id.btnSubmit);
+    }
+
+    private void initRegister() {
+        Student student = null;
+
+        if (editName.getText().toString() == "") {
+            txtWarnName.setVisibility(View.VISIBLE);
+        }
+        if (editEmail.getText().toString() == "") {
+            txtWarnEmail.setVisibility(View.VISIBLE);
+        }
+        if (editPhone.getText().toString() == "") {
+            txtWarnMobile.setVisibility(View.VISIBLE);
+        }
+
+
+        try {
+            student = new Student(editName.getText().toString(),
+                    editEmail.getText().toString(),
+                    editPhone.getText().toString(),
+                    1,
+                    'A');
+
+            txtWarnName.setVisibility(View.INVISIBLE);
+            txtWarnEmail.setVisibility(View.INVISIBLE);
+            txtWarnMobile.setVisibility(View.INVISIBLE);
+
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "Please check your input", Toast.LENGTH_SHORT).show();
+        }
+
+        Toast.makeText(MainActivity.this, "Data Not Submitted" + student.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void showSnackBar(String message, String actionName) {
+        txtWarnName.setVisibility(View.GONE);
+        txtWarnEmail.setVisibility(View.GONE);
+        txtWarnMobile.setVisibility(View.GONE);
+        txtWarnAgreement.setVisibility(View.GONE);
+
+        Snackbar.make(parent, message, Snackbar.LENGTH_INDEFINITE)
+                .setAction(actionName, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }).show();
+    }
+
+    private boolean validateData() {
+        //TODO implement a data checking algo
+        return true;
+    }
+
+    private boolean submitData() {
+        //TODO implement a data submission algo
+        return true;
     }
 }
---
+++ app/src/main/java/com/tushgaurav/registerrooms/Student.java
@@ -0,0 +1,69 @@
+package com.tushgaurav.registerrooms;
+
+public class Student {
+    private String name, email, phone;
+    private int floor;
+    private char block;
+
+    public Student(String name, String email, String phone, int floor, char block) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.floor = floor;
+        this.block = block;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", floor=" + floor +
+                ", block=" + block +
+                '}';
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public char getBlock() {
+        return block;
+    }
+
+    public void setBlock(char block) {
+        this.block = block;
+    }
+
+
+}
