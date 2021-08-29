af34243b964dd43abaa6bb9f127d7faab3f358f5
Tushar <tushar.gaurav416@gmail.com>
2021-08-29 20:11:17 +0530

    fixed bugs

--- app/src/main/java/com/tushgaurav/registerrooms/ViewStudents.java
+++ app/src/main/java/com/tushgaurav/registerrooms/ViewStudents.java
@@ -2,13 +2,27 @@ package com.tushgaurav.registerrooms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewStudents extends AppCompatActivity {
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_students);
-    }
+
+//        btnBack = findViewById(R.id.btnBack);
+
+//        btnBack.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(ViewStudents.this, MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 }
